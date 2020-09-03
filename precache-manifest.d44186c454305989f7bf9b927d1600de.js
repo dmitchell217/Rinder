afbf28346f34b0d86a06700023e29a38dc11d69f
David Mitchell <dmitchell217@gmail.com>
2020-09-03 19:54:40 -0400

    Updates

---
+++ precache-manifest.d44186c454305989f7bf9b927d1600de.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5128a5ac534237da4460ba702c405e8",
+    "url": "/rinder/index.html"
+  },
+  {
+    "revision": "59a866d737879e574979",
+    "url": "/rinder/static/css/2.ee241de9.chunk.css"
+  },
+  {
+    "revision": "b1bfbac53069820c6fde",
+    "url": "/rinder/static/css/main.6b27cbad.chunk.css"
+  },
+  {
+    "revision": "59a866d737879e574979",
+    "url": "/rinder/static/js/2.01caf950.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/rinder/static/js/2.01caf950.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b1bfbac53069820c6fde",
+    "url": "/rinder/static/js/main.1bfeedce.chunk.js"
+  },
+  {
+    "revision": "3d84ec87de6bcd712fe3",
+    "url": "/rinder/static/js/runtime-main.35ac4ebd.js"
+  },
+  {
+    "revision": "4c79eb4f5ddcb2fbb9a7b0302c0b989d",
+    "url": "/rinder/static/media/badLogo.4c79eb4f.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/rinder/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "265e607140ccbde56c659b2c6a934875",
+    "url": "/rinder/static/media/mockGraph.265e6071.png"
+  }
+]);
